e13f7fa0e66215e110a58e25a57effb65544d7b4
Santiago Zetina <zetinasanti@gmail.com>
2023-04-17 15:06:09 -0400

    test

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ async function startServer() {
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/'));
 });
 
 
